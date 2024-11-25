c511029844866765e2c185dee82df451ed5b8b71
Talhadmr <demirtalha093@gmail.com>
2024-11-25 23:23:59 +0300

    some minor changes for p1 and initial commit for p2

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -3,5 +3,7 @@ sudo apt update
 sudo apt install -y curl
 export K3S_TOKEN=$(cat /vagrant/agent-token.env)
 export K3S_URL="https://192.168.56.110:6443"
+#this line is for the worker node to have a static IP
+export INSTALL_K3S_EXEC="--node-ip=192.168.56.111"
 curl -sfL https://get.k3s.io | sh -
 rm -f /vagrant/agent-token.env
