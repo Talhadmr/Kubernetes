d3ed0324fdbed9c3d372dc2dda3e956ed4f95752
mtalhaaygen <mtalha.aygen18@gmail.com>
2025-01-12 12:00:25 +0300

    apply

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -4,3 +4,8 @@ sudo apt install -y curl
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --bind-address=192.168.56.110 --advertise-address=192.168.56.110 --node-ip=192.168.56.110"
 curl -sfL https://get.k3s.io | sh -
 echo 'alias k="kubectl"' >> /home/vagrant/.bashrc
+
+/usr/local/bin/kubectl apply -f /vagrant/manifest/app1-deployment.yaml
+/usr/local/bin/kubectl apply -f /vagrant/manifest/app2-deployment.yaml
+/usr/local/bin/kubectl apply -f /vagrant/manifest/app3-deployment.yaml
+/usr/local/bin/kubectl apply -f /vagrant/manifest/ingress.yaml
